6cc76f3ee9fbdf63c1ddb692fe18affcf7ff981b
FuexFollets <fuex9follets@gmail.com>
2022-12-14 19:49:12 -0500

    Sync

--- Units/unit.hpp
+++ Units/unit.hpp
@@ -5,45 +5,9 @@
 #include <concepts>
 #include <type_traits>
 
+#include "unit_options.hpp"
 
 namespace unit {
-
-    /* Enum of quantative unit types */
-    enum QuantitativeType { 
-        Whole = std::uintmax_t,
-        Integer = std::intmax_t,
-        Continuous = long double,
-    };
-
-
-    /* Checks if a integer is a integer power of 10 */
-    template <std::integral T>
-        constexpr bool constexpr_power_of_10(T whole_number) {
-        if (whole_number == 1) return true;
-
-        while (whole_number != 0) {
-            if (whole_number == 1) return true;
-            if (whole_number % 10 != ) return false;
-
-            whole_number /= 10;
-        }
-        return false;
-    }
-
-
-    /* Indicator for no metric prefix */
-    struct no_prefix {};
-
-
-    /* Metric prefix ratio concept (ex: std::milli, std::tera) */
-    template <typename T>
-        concept metric_prefix_ratio = requires {
-            T::num;
-            T::den;
-        } &&
-        constexpr_power_of_10(T::num) && constexpr_power_of_10(T::den) ||
-        std::same_as<T, no_prefix>;
-
 
     /* Checks if a type is a specialization of a unit template */
     template <
@@ -66,8 +30,8 @@ namespace unit {
 
 
     template <
-        template <int, metric_prefix_ratio> UT1,
-        template <int, metric_prefix_ratio> UT2
+        template <int, metric_prefix_ratio> class UT1,
+        template <int, metric_prefix_ratio> class UT2
     > struct is_same_unit_template : std::false_type {};
 
     template <
@@ -79,16 +43,16 @@ namespace unit {
     template <typename T>
         struct is_unit {
             static const bool value {
-                requires { requires T::is_unit }
+                requires { requires T::is_unit; }
             };
         };
 
     // Concept implementation of 'is_unit'
     template <typename T>
-        concept unit = is_unit<T>;
+        concept unit = is_unit<T>::value;
 
 
-    template <unit T, unit V>
+    template <typename T, typename V>
         concept same_unit_different_metric_prefix = 
         is_same_unit_template<
             T::derived_basic_unit_template,
@@ -96,8 +60,8 @@ namespace unit {
         >::value &&
 
         !std::is_same_v<
-            T::derived_metric_prefix,
-            V::derived_metric_prefix
+            typename T::derived_metric_prefix,
+            typename V::derived_metric_prefix
         >;
         
 
@@ -127,10 +91,8 @@ namespace unit {
 
     /* Inheritable template struct for implementing unit templates */
     template <
-        template <int, metric_prefix_ratio> struct derived_template,
-        QuantitativeType Qt,
-        int exp_power = 1,
-        metric_prefix_ratio R = no_prefix
+        template <basic_unit_option> typename derived_template,
+        
     >
     struct basic_unit :
         std::conditional<
